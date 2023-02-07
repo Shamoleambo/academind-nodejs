bfbaeedcd40955b0b16b12a467ffee7fc7d98d31
Tiago Gomes <tidgomes@gmail.com>
2023-02-07 12:32:01 -0300

    use fileSystem and redirect to Location '/' after filling the form

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const http = require('http')
+const fs = require('fs')
 
 const server = http.createServer((req, res) => {
   if (req.url === '/') {
@@ -10,11 +11,14 @@ const server = http.createServer((req, res) => {
     res.write('</html>')
     return res.end()
   }
+  if (req.url === '/message' && req.method === 'POST') {
+    fs.writeFileSync('newfile.txt', 'Down with the sickness!!')
+    res.writeHead(302, { Location: '/' })
+    return res.end()
+  }
   res.write('<html>')
   res.write('<head><title>Enter message</title></head>')
-  res.write(
-    '<body><h1>Hello neighbor!</h1></body>'
-  )
+  res.write('<body><h1>Hello neighbor!</h1></body>')
   res.write('</html>')
   res.end()
 })
