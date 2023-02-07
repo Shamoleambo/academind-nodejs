5c17a6d728eb7c7d1cc2fccab9278a7a83dcbe97
Tiago Gomes <tidgomes@gmail.com>
2023-02-07 12:24:19 -0300

    create route and simple form

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 const http = require('http')
 
 const server = http.createServer((req, res) => {
-  res.setHeader('Content-Type', 'text/html')
+  if (req.url === '/') {
+    res.write('<html>')
+    res.write('<head><title>Enter message</title></head>')
+    res.write(
+      '<body><form action="/message" method="POST"><input type="text" name="message" /><button type="submit">Send</button></body>'
+    )
+    res.write('</html>')
+    return res.end()
+  }
   res.write('<html>')
-  res.write('<head><title>Title</title></head>')
-  res.write('<body><h1>Hello from the body!</h1></body>')
+  res.write('<head><title>Enter message</title></head>')
+  res.write(
+    '<body><h1>Hello neighbor!</h1></body>'
+  )
   res.write('</html>')
-  res.end('')
+  res.end()
 })
 
 server.listen(3000)
