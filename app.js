40dd12b15f396fb3064a55ed64a365aa6a366bdc
Tiago Gomes <tidgomes@gmail.com>
2023-02-07 14:51:50 -0300

    parse request data using chunk

--- app.js
+++ app.js
@@ -12,7 +12,16 @@ const server = http.createServer((req, res) => {
     return res.end()
   }
   if (req.url === '/message' && req.method === 'POST') {
-    fs.writeFileSync('newfile.txt', 'Down with the sickness!!')
+    const body = []
+    req.on('data', chunk => {
+      console.log(chunk)
+      body.push(chunk)
+    })
+    req.on('end', () => {
+      const parsedBody = Buffer.concat(body).toString()
+      const message = parsedBody.split('=')[1]
+      fs.writeFileSync('newfile.txt', message)
+    })
     res.writeHead(302, { Location: '/' })
     return res.end()
   }
