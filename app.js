a50d2fb358af6119307b12add88fec766f9be8b2
Tiago Gomes <tidgomes@gmail.com>
2023-02-07 12:14:22 -0300

    send data on the response

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const http = require('http')
 
-const app = http.createServer((req, res) => {
-  console.log(req)
+const server = http.createServer((req, res) => {
+  res.setHeader('Content-Type', 'text/html')
+  res.write('<html>')
+  res.write('<head><title>Title</title></head>')
+  res.write('<body><h1>Hello from the body!</h1></body>')
+  res.write('</html>')
+  res.end('')
 })
 
-app.listen(3000)
+server.listen(3000)
