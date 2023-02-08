000f886d6862b96d5a4b8715d3ccce238b10d74b
Tiago Gomes <tidgomes@gmail.com>
2023-02-08 11:10:19 -0300

    implement express instead of plain node

--- app.js
+++ app.js
@@ -1,6 +1,23 @@
-const http = require('http')
-const routes = require('./routes')
+const express = require('express')
+const bodyParser = require('body-parser')
 
-const server = http.createServer(routes)
+const app = express()
 
-server.listen(3000)
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.use('/add-product', (req, res) => {
+  res.send(
+    '<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>'
+  )
+})
+
+app.use('/product', (req, res) => {
+  console.log(req.body)
+  res.redirect('/')
+})
+
+app.use('/', (req, res) => {
+  res.send('<h1>Hello World!</h1>')
+})
+
+app.listen(3000)
