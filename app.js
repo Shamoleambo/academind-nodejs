32d7356f09bd8502be1a82d1356d5e244b99a5e8
Tiago Gomes <tidgomes@gmail.com>
2023-02-08 15:25:41 -0300

    share data between files

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const path = require('path')
 const express = require('express')
 const bodyParser = require('body-parser')
-const adminRoutes = require('./routes/admin')
+const adminData = require('./routes/admin')
 const shopRoutes = require('./routes/shop')
 
 const app = express()
@@ -9,7 +9,7 @@ const app = express()
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.use('/admin', adminRoutes)
+app.use('/admin', adminData.routes)
 app.use(shopRoutes)
 app.use((req, res) => {
   res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
--- routes/shop.js
+++ routes/shop.js
@@ -1,10 +1,12 @@
 const path = require('path')
 const express = require('express')
+const adminData = require('./admin')
 const rootDir = require('../utils/path')
 
 const router = express.Router()
 
 router.get('/', (req, res) => {
+  console.log(adminData.products)
   res.sendFile(path.join(rootDir, 'views', 'shop.html'))
 })
 
