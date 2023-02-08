dc48d33a43a3711e5043831c06e5e85f56d8042d
Tiago Gomes <tidgomes@gmail.com>
2023-02-08 12:40:33 -0300

    create and serve 404 page

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const express = require('express')
 const bodyParser = require('body-parser')
 const adminRoutes = require('./routes/admin')
@@ -7,10 +8,10 @@ const app = express()
 
 app.use(bodyParser.urlencoded({ extended: false }))
 
-app.use('/admin' ,adminRoutes)
+app.use('/admin', adminRoutes)
 app.use(shopRoutes)
 app.use((req, res) => {
-  res.status(404).send('<h1>Page not found</h1>')
+  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
 })
 
 app.listen(3000)
