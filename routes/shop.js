3618b30fbc48ca676fa7251344238d024c948168
Tiago Gomes <tidgomes@gmail.com>
2023-02-08 15:57:42 -0300

    create pug template for shop

--- routes/shop.js
+++ routes/shop.js
@@ -6,8 +6,7 @@ const rootDir = require('../utils/path')
 const router = express.Router()
 
 router.get('/', (req, res) => {
-  console.log(adminData.products)
-  res.sendFile(path.join(rootDir, 'views', 'shop.html'))
+  res.render('shop')
 })
 
 module.exports = router
