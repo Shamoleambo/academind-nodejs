d54dfd37fc51a0942938d5c72977b7430a35f996
Tiago Gomes <tidgomes@gmail.com>
2023-02-10 15:05:02 -0300

    use controller in router

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const path = require('path')
 const express = require('express')
 const bodyParser = require('body-parser')
-const adminData = require('./routes/admin')
+const adminRoutes = require('./routes/admin')
 const shopRoutes = require('./routes/shop')
 
 const app = express()
@@ -12,10 +12,10 @@ app.set('views', 'views')
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.use('/admin', adminData.routes)
+app.use('/admin', adminRoutes)
 app.use(shopRoutes)
 app.use((req, res) => {
   res.status(404).render('404', { pageTitle: 'Page Not Found' })
 })
 
-app.listen(3000)
+app.listen(3000, () => console.log('Your applicaion is running on port 3000'))
--- routes/shop.js
+++ routes/shop.js
@@ -1,20 +1,9 @@
 const path = require('path')
 const express = require('express')
-const adminData = require('./admin')
-const rootDir = require('../utils/path')
+const productsController = require('../controller/products')
 
 const router = express.Router()
 
-router.get('/', (req, res) => {
-  const products = adminData.products
-  res.render('shop', {
-    products,
-    pageTitle: 'Shop',
-    path: '/',
-    hasProducts: products.length > 0,
-    activeShop: true,
-    productCSS: true
-  })
-})
+router.get('/', productsController.getProducts)
 
 module.exports = router
