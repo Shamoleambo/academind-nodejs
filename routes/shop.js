83bf95cb1d72acfa984c4b57dc02d65f3cc81f87
Tiago Gomes <tidgomes@gmail.com>
2023-02-13 11:05:32 -0300

    add index and cart routes

--- routes/shop.js
+++ routes/shop.js
@@ -4,6 +4,8 @@ const productsController = require('../controller/products')
 
 const router = express.Router()
 
-router.get('/', productsController.getProducts)
+router.get('/', productsController.getShopPage)
+router.get('/products', productsController.getProducts)
+router.get('/cart', productsController.getCart)
 
 module.exports = router
