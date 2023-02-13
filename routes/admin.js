f6dac6b101856937a1eb1e215b065a94e8681e45
Tiago Gomes <tidgomes@gmail.com>
2023-02-13 11:06:04 -0300

    add admin products route

--- routes/admin.js
+++ routes/admin.js
@@ -5,7 +5,7 @@ const productsController = require('../controller/products')
 const router = express.Router()
 
 router.get('/add-product', productsController.getAddProduct)
-
 router.post('/add-product', productsController.postAddProduct)
+router.get('/products', productsController.getAdminProducts)
 
 module.exports = router
