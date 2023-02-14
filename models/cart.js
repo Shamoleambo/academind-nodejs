e28ce57534b9b9e488d876cc71f95d06b2aaf8c1
Tiago Gomes <tidgomes@gmail.com>
2023-02-14 12:14:04 -0300

    create Cart Model function to retrieve cart items

--- models/cart.js
+++ models/cart.js
@@ -33,6 +33,17 @@ module.exports = class Cart {
     })
   }
 
+  static getCart(cb) {
+    fs.readFile(p, (err, fileContent) => {
+      const cart = JSON.parse(fileContent)
+      if (err) {
+        cb(null)
+      } else {
+        cb(cart)
+      }
+    })
+  }
+
   static deleteProduct(id, productPrice) {
     fs.readFile(p, (err, fileContent) => {
       if (err) return
