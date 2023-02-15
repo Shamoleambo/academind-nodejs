f5dae5633be4e2c857f0e01f20c682157680a15c
Tiago Gomes <tidgomes@gmail.com>
2023-02-15 10:36:51 -0300

    create a new entry with sequelize

--- controller/admin.js
+++ controller/admin.js
@@ -10,9 +10,17 @@ exports.getAddProduct = (req, res) => {
 
 exports.postAddProduct = (req, res) => {
   const title = req.body.title
+  const price = req.body.price
   const imageUrl = req.body.imageUrl
   const description = req.body.description
-  const price = req.body.price
+  Product.create({
+    title,
+    price,
+    imageUrl,
+    description
+  })
+    .then(result => console.log('Product Created'))
+    .catch(err => console.log(err))
 
   const product = new Product(null, title, imageUrl, description, price)
   product
