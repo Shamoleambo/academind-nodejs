9cb5bd11913e02fec44da293dc7578916d0a5d01
Tiago Gomes <tidgomes@gmail.com>
2023-02-16 14:09:55 -0300

    establish connection to mongodb database

--- app.js
+++ app.js
@@ -2,15 +2,9 @@ const path = require('path')
 const express = require('express')
 const bodyParser = require('body-parser')
 const errorController = require('./controller/error')
-const adminRoutes = require('./routes/admin')
-const shopRoutes = require('./routes/shop')
-const sequelize = require('./utils/database')
-const Product = require('./models/product')
-const User = require('./models/user')
-const Cart = require('./models/cart')
-const CartItem = require('./models/cart-item')
-const Order = require('./models/order')
-const OrderItem = require('./models/order-item')
+// const adminRoutes = require('./routes/admin')
+// const shopRoutes = require('./routes/shop')
+const mongoConnect = require('./utils/database')
 
 const app = express()
 
@@ -21,45 +15,19 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.use((req, res, next) => {
-  User.findByPk(1)
-    .then(user => {
-      req.user = user
-      next()
-    })
-    .catch(err => console.log(err))
+  // User.findByPk(1)
+  //   .then(user => {
+  //     req.user = user
+  //     next()
+  //   })
+  //   .catch(err => console.log(err))
 })
 
-app.use('/admin', adminRoutes)
-app.use(shopRoutes)
+// app.use('/admin', adminRoutes)
+// app.use(shopRoutes)
 app.use(errorController.get404)
 
-Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' })
-//The following is optional, since I can establish the relation between User and Product any way around
-User.hasMany(Product)
-User.hasOne(Cart)
-Cart.belongsToMany(Product, { through: CartItem })
-Product.belongsToMany(Cart, { through: CartItem })
-User.hasMany(Order)
-Order.belongsToMany(Product, { through: OrderItem })
-
-sequelize
-  // .sync({ force: true })
-  .sync()
-  .then(result => {
-    return User.findByPk(1)
-  })
-  .then(user => {
-    if (!user) {
-      return User.create({ name: 'Pepe', email: 'pepe@mail.com' })
-    }
-    return user
-  })
-  .then(user => {
-    return user.createCart()
-  })
-  .then(cart => {
-    app.listen(3000, () =>
-      console.log('Your applicaion is running on port 3000')
-    )
-  })
-  .catch(err => console.log(err))
+mongoConnect(client => {
+  console.log(client)
+  app.listen(3000)
+})
