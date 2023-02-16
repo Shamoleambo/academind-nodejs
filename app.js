96ebf0150685042714c245efee81418679229a0c
Tiago Gomes <tidgomes@gmail.com>
2023-02-16 16:54:56 -0300

    handle shop route

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const errorController = require('./controller/error')
 const adminRoutes = require('./routes/admin')
-// const shopRoutes = require('./routes/shop')
+const shopRoutes = require('./routes/shop')
 const { mongoConnect } = require('./utils/database')
 
 const app = express()
@@ -15,7 +15,7 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/admin', adminRoutes)
-// app.use(shopRoutes)
+app.use(shopRoutes)
 app.use(errorController.get404)
 
 mongoConnect(() => {
