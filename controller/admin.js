53ba81f3919d65fb9614794e9e19c45be757a0cb
Tiago Gomes <tidgomes@gmail.com>
2023-02-16 16:23:00 -0300

    refactor postAddProduct according to mongodb

--- controller/admin.js
+++ controller/admin.js
@@ -1,7 +1,26 @@
+const Product = require('../models/product')
+
 exports.getAddProduct = (req, res) => {
   res.render('admin/edit-product', {
     pageTitle: 'Add Product',
     path: '/admin/add-product',
     editing: false
   })
+}
+
+exports.postAddProduct = (req, res) => {
+  const title = req.body.title
+  const price = req.body.price
+  const description = req.body.description
+  const imageUrl = req.body.imageUrl
+
+  const product = new Product(title, price, description, imageUrl)
+
+  product
+    .save()
+    .then(result => {
+      console.log('Product Created')
+      res.redirect('/admin/products')
+    })
+    .catch(err => console.log(err))
 }
