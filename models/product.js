60a3091004265e08e23a581943efaa3aa40338e4
Tiago Gomes <tidgomes@gmail.com>
2023-02-17 13:25:41 -0300

    fix creation of new product

--- models/product.js
+++ models/product.js
@@ -1,12 +1,14 @@
 const mongodb = require('mongodb')
 const { getDb } = require('../utils/database')
 class Product {
-  constructor(title, price, description, imageUrl, id) {
+  constructor(title, price, description, imageUrl, id = null) {
     this.title = title
     this.price = price
     this.description = description
     this.imageUrl = imageUrl
-    this._id = new mongodb.ObjectId(id)
+    if (id) {
+      this._id = new mongodb.ObjectId(id)
+    }
   }
 
   save() {
