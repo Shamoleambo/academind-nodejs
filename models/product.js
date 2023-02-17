8bbee2d043b91f1f32be18fc9365cb9f642f7c8d
Tiago Gomes <tidgomes@gmail.com>
2023-02-17 12:02:32 -0300

    add edit logic to save method in Product Model

--- models/product.js
+++ models/product.js
@@ -1,22 +1,29 @@
 const mongodb = require('mongodb')
 const { getDb } = require('../utils/database')
 class Product {
-  constructor(title, price, description, imageUrl) {
+  constructor(title, price, description, imageUrl, id) {
     this.title = title
     this.price = price
     this.description = description
     this.imageUrl = imageUrl
+    this._id = id
   }
 
   save() {
     const db = getDb()
-    return db
-      .collection('products')
-      .insertOne(this)
-      .then(result => {
-        console.log(result)
-      })
-      .catch(err => console.log(err))
+    if (this._id) {
+      return db
+        .collection('products')
+        .updateOne({ _id: new mongodb.ObjectId(this._id) }, { $set: this })
+    } else {
+      return db
+        .collection('products')
+        .insertOne(this)
+        .then(result => {
+          console.log(result)
+        })
+        .catch(err => console.log(err))
+    }
   }
 
   static fetchAll() {
