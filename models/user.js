6a765802bde354e3a382430c284f0e01499ccf94
Tiago Gomes <tidgomes@gmail.com>
2023-02-17 15:50:16 -0300

    create save and findById methods for User

--- models/user.js
+++ models/user.js
@@ -1,21 +1,23 @@
-const Sequelize = require('sequelize')
-const sequelize = require('../utils/database')
+const mongodb = require('mongodb')
+const { getDb } = require('../utils/database')
 
-const User = sequelize.define('user', {
-  id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true
-  },
-  name: {
-    type: Sequelize.STRING,
-    allowNull: false
-  },
-  email: {
-    type: Sequelize.STRING,
-    allowNull: false
+class User {
+  constructor(name, email) {
+    this.name = name
+    this.email = email
   }
-})
+
+  save() {
+    const db = getDb()
+
+    return db.collection('users').insertOne(this)
+  }
+
+  static findById(prodId) {
+    const userObjectId = new mongodb.ObjectId(prodId)
+    const db = getDb()
+    return db.collection('users').findOne({_id: userObjectId})
+  }
+}
 
 module.exports = User
