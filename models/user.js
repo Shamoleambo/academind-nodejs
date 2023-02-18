677181c320517c6eea117571f1d5c900067dd87c
Tiago Gomes <tidgomes@gmail.com>
2023-02-18 13:17:34 -0300

    create orders from cart method added to User Model

--- models/user.js
+++ models/user.js
@@ -77,6 +77,23 @@ class User {
       )
   }
 
+  addOrder() {
+    const db = getDb()
+    return db
+      .collection('orders')
+      .insertOne(this.cart)
+      .then(() => {
+        this.cart = []
+        return db.collection('users').updateOne(
+          { _id: new mongodb.ObjectId(this._id) },
+          { $set: { cart: { items: [] } } }
+        )
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }
+
   static findById(prodId) {
     const db = getDb()
     const userObjectId = new mongodb.ObjectId(prodId)
