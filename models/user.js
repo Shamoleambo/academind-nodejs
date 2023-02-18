fec3f4720cffafdcb47460e7e068ee874a6126df
Tiago Gomes <tidgomes@gmail.com>
2023-02-18 10:36:15 -0300

    add cart and id fields to user and create addToCart method

--- models/user.js
+++ models/user.js
@@ -2,15 +2,35 @@ const mongodb = require('mongodb')
 const { getDb } = require('../utils/database')
 
 class User {
-  constructor(name, email) {
+  constructor(name, email, cart, id) {
     this.name = name
     this.email = email
+    this.cart = cart
+    this._id = id
   }
 
   save() {
     const db = getDb()
 
     return db.collection('users').insertOne(this)
+  }
+
+  addToCart(product) {
+    // const cartProductIndex = this.cart.items.findIndex(
+    //   cartProduct => cartProduct._id === product._id
+    // )
+
+    const updatedCart = {
+      items: [{ productId: new mongodb.ObjectId(product._id), quantity: 1 }]
+    }
+
+    const db = getDb()
+    return db
+      .collection('users')
+      .updateOne(
+        { _id: new mongodb.ObjectId(this._id) },
+        { $set: { cart: updatedCart } }
+      )
   }
 
   static findById(prodId) {
