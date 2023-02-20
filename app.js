7f4826727e3276699a493cf693291313fb11c03d
Tiago Gomes <tidgomes@gmail.com>
2023-02-20 11:54:59 -0300

    add user field with user schema in the request

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const dotenv = require('dotenv')
 const errorController = require('./controller/error')
 const adminRoutes = require('./routes/admin')
 const shopRoutes = require('./routes/shop')
+const User = require('./models/user')
 
 const app = express()
 
@@ -13,6 +14,17 @@ dotenv.config()
 
 app.set('view engine', 'ejs')
 app.set('views', 'views')
+
+app.use((req, res, next) => {
+  User.findById('63f3874cca9d5f7f39325d66')
+    .then(user => {
+      req.user = user
+      next()
+    })
+    .catch(err => {
+      console.log(err)
+    })
+})
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static(path.join(__dirname, 'public')))
@@ -25,7 +37,17 @@ mongoose
   .connect(
     `mongodb+srv://tidgomes:${process.env.DB_PASSWORD}@cluster0.gomczzm.mongodb.net/shop?retryWrites=true&w=majority`
   )
-  .then(result => {
+  .then(() => {
+    User.findOne().then(user => {
+      if (!user) {
+        const user = new User({
+          name: 'Test User',
+          email: 'testuser@mail.com',
+          cart: { items: [] }
+        })
+        user.save()
+      }
+    })
     console.log('Connected to the database')
     app.listen(3000)
   })
