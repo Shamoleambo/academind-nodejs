ccae88a01517fa22363fc04d0159a6a328917e6c
Tiago Gomes <tidgomes@gmail.com>
2023-02-20 11:05:12 -0300

    turn off routes temporarily for mongoose required refactoring

--- controller/admin.js
+++ controller/admin.js
@@ -14,14 +14,7 @@ exports.postAddProduct = (req, res) => {
   const description = req.body.description
   const imageUrl = req.body.imageUrl
 
-  const product = new Product(
-    title,
-    price,
-    description,
-    imageUrl,
-    null,
-    req.user._id
-  )
+  const product = new Product({ title, price, description, imageUrl })
 
   product
     .save()
