c3531b9851a43c5c81ac7928bac65c6a91841486
Tiago Gomes <tidgomes@gmail.com>
2023-02-20 10:50:59 -0300

    create Product Model using mongoose Schema

--- models/product.js
+++ models/product.js
@@ -1,53 +1,66 @@
-const mongodb = require('mongodb')
-const { getDb } = require('../utils/database')
-class Product {
-  constructor(title, price, description, imageUrl, id = null, userId) {
-    this.title = title
-    this.price = price
-    this.description = description
-    this.imageUrl = imageUrl
-    this.userId = userId
-    if (id) {
-      this._id = new mongodb.ObjectId(id)
-    }
-  }
+const mongoose = require('mongoose')
 
-  save() {
-    const db = getDb()
-    if (this._id) {
-      return db
-        .collection('products')
-        .updateOne({ _id: this._id }, { $set: this })
-    } else {
-      return db
-        .collection('products')
-        .insertOne(this)
-        .then(result => {
-          console.log(result)
-        })
-        .catch(err => console.log(err))
-    }
-  }
+const Schema = mongoose.Schema
 
-  static fetchAll() {
-    const db = getDb()
-    return db.collection('products').find().toArray()
-  }
+const productSchema = new Schema({
+  title: { type: String, required: true },
+  price: { type: Number, required: true },
+  description: { type: String, required: true },
+  imageUrl: { type: String, required: true }
+})
 
-  static findById(productId) {
-    const db = getDb()
-    return db
-      .collection('products')
-      .find({ _id: new mongodb.ObjectId(productId) })
-      .next()
-  }
+module.exports = mongoose.model('Product', productSchema)
 
-  static deleteById(productId) {
-    const db = getDb()
-    return db
-      .collection('products')
-      .deleteOne({ _id: new mongodb.ObjectId(productId) })
-  }
-}
+// const mongodb = require('mongodb')
+// const { getDb } = require('../utils/database')
+// class Product {
+//   constructor(title, price, description, imageUrl, id = null, userId) {
+//     this.title = title
+//     this.price = price
+//     this.description = description
+//     this.imageUrl = imageUrl
+//     this.userId = userId
+//     if (id) {
+//       this._id = new mongodb.ObjectId(id)
+//     }
+//   }
 
-module.exports = Product
+//   save() {
+//     const db = getDb()
+//     if (this._id) {
+//       return db
+//         .collection('products')
+//         .updateOne({ _id: this._id }, { $set: this })
+//     } else {
+//       return db
+//         .collection('products')
+//         .insertOne(this)
+//         .then(result => {
+//           console.log(result)
+//         })
+//         .catch(err => console.log(err))
+//     }
+//   }
+
+//   static fetchAll() {
+//     const db = getDb()
+//     return db.collection('products').find().toArray()
+//   }
+
+//   static findById(productId) {
+//     const db = getDb()
+//     return db
+//       .collection('products')
+//       .find({ _id: new mongodb.ObjectId(productId) })
+//       .next()
+//   }
+
+//   static deleteById(productId) {
+//     const db = getDb()
+//     return db
+//       .collection('products')
+//       .deleteOne({ _id: new mongodb.ObjectId(productId) })
+//   }
+// }
+
+// module.exports = Product
