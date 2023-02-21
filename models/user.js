416b91d80fb0a296d2d704bca57864cd0de1346a
Tiago Gomes <tidgomes@gmail.com>
2023-02-21 12:20:40 -0300

    add method addToCart to the User Model

--- models/user.js
+++ models/user.js
@@ -19,6 +19,30 @@ const userSchema = new Schema({
   }
 })
 
+userSchema.methods.addToCart = function (product) {
+  const cartProductIndex = this.cart.items.findIndex(
+    cartProduct => cartProduct.productId.toString() === product._id.toString()
+  )
+
+  const updatedCartItems = this.cart.items
+
+  if (cartProductIndex >= 0) {
+    updatedCartItems[cartProductIndex].quantity += 1
+  } else {
+    updatedCartItems.push({
+      productId: product._id,
+      quantity: 1
+    })
+  }
+
+  const updatedCart = {
+    items: updatedCartItems
+  }
+
+  this.cart = updatedCart
+  return this.save()
+}
+
 module.exports = mongoose.model('User', userSchema)
 
 // const mongodb = require('mongodb')
