5caaaea18acac195bb6f12cb87a9c6f4a80b2bcb
Tiago Gomes <tidgomes@gmail.com>
2023-02-24 13:54:21 -0300

    create postLogout and add the session to the request with isLoggedIn and user fields

--- controller/auth.js
+++ controller/auth.js
@@ -1,13 +1,28 @@
+const User = require('../models/user')
+
 exports.getLogin = (req, res, next) => {
-  const isLoggedIn = req.get('Cookie').split(';')[0].trim().split('=')[1]
   res.render('auth/login', {
     pageTitle: 'Login',
     path: '/login',
-    isAuthenticated: isLoggedIn
+    isAuthenticated: false
   })
 }
 
 exports.postLogin = (req, res, next) => {
-  res.setHeader('Set-Cookie', 'isLoggedIn=true')
-  res.redirect('/')
+  User.findById('63f8d977cffbf8b3f87ddd7e')
+    .then(user => {
+      req.session.isLoggedIn = true
+      req.session.user = user
+      res.redirect('/')
+    })
+    .catch(err => {
+      console.log(err)
+    })
+}
+
+exports.postLogout = (req, res, next) => {
+  req.session.destroy(err => {
+    console.log(err)
+    res.redirect('/')
+  })
 }
