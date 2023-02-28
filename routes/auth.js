13cedad3be15a301eb7a9b27e83783c25fdd8caa
Tiago Gomes <tidgomes@gmail.com>
2023-02-28 10:54:58 -0300

    create signup display and route

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,17 @@
-const express = require('express')
-const authController = require('../controller/auth')
+const express = require('express');
 
-const router = express.Router()
+const authController = require('../controller/auth');
 
-router.get('/login', authController.getLogin)
-router.post('/login', authController.postLogin)
-router.post('/logout', authController.postLogout)
+const router = express.Router();
 
-module.exports = router
+router.get('/login', authController.getLogin);
+
+router.get('/signup', authController.getSignup);
+
+router.post('/login', authController.postLogin);
+
+router.post('/signup', authController.postSignup);
+
+router.post('/logout', authController.postLogout);
+
+module.exports = router;
