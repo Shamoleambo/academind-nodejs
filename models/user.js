e10a18163a1d792cd1e16ea2c06ebc481a66034a
Tiago Gomes <tidgomes@gmail.com>
2023-02-28 12:00:36 -0300

    modify User model to have a password instead of a name

--- models/user.js
+++ models/user.js
@@ -4,8 +4,8 @@ const Product = require('./product')
 const Schema = mongoose.Schema
 
 const userSchema = new Schema({
-  name: { type: String, required: true },
   email: { type: String, required: true },
+  password: { type: String, required: true },
   cart: {
     items: [
       {
