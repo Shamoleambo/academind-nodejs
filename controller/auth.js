df7148ee3478536b94f3ec5fd6ca79fb7412c34d
Tiago Gomes <tidgomes@gmail.com>
2023-03-01 14:34:15 -0300

    check for errors during the validation and render the correct output

--- controller/auth.js
+++ controller/auth.js
@@ -40,6 +40,18 @@ exports.getSignup = (req, res, next) => {
 exports.postLogin = (req, res, next) => {
   const email = req.body.email
   const password = req.body.password
+  const errors = validationResult(req)
+
+  if (!errors.isEmpty()) {
+    res
+      .status(422)
+      .render('auth/login', {
+        path: '/login',
+        pageTitle: 'Login',
+        errorMessage: errors.array()[0].msg
+      })
+  }
+
   User.findOne({ email })
     .then(user => {
       if (!user) {
