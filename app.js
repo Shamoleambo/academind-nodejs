d85956d46d18063d70cfd09479b3f7a7e5c8465f
Tiago Gomes <tidgomes@gmail.com>
2023-03-01 11:25:11 -0300

    readd the user to the request

--- app.js
+++ app.js
@@ -44,6 +44,21 @@ app.use((req, res, next) => {
 })
 app.use(flash())
 
+app.use((req, res, next) => {
+  if (!req.session.user._id) {
+    return next()
+  }
+
+  User.findOne({ _id: req.session.user._id })
+    .then(user => {
+      req.user = user
+      next()
+    })
+    .catch(err => {
+      console.log(err)
+    })
+})
+
 app.use('/admin', adminRoutes)
 app.use(shopRoutes)
 app.use(authRoutes)
