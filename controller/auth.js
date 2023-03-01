9676b35f507b0afa19611e82524a63ae9902b4d8
Tiago Gomes <tidgomes@gmail.com>
2023-03-01 11:07:47 -0300

    post a new password to be changed in the db

--- controller/auth.js
+++ controller/auth.js
@@ -187,8 +187,38 @@ exports.getNewPassword = (req, res, next) => {
         path: '/new-password',
         pageTitle: 'New Password',
         errorMessage: message,
-        userId: user._id.toString()
+        userId: user._id.toString(),
+        passwordToken: token
       })
+    })
+    .catch(err => {
+      console.log(err)
+    })
+}
+
+exports.postNewPassword = (req, res, next) => {
+  const newPassword = req.body.password
+  const userId = req.body.userId
+  const passwordToken = req.body.passwordToken
+  let resetUser
+
+  User.findOne({
+    resetToken: passwordToken,
+    resetTokenExpiration: { $gt: Date.now() },
+    _id: userId
+  })
+    .then(user => {
+      resetUser = user
+      return bcrypt.hash(newPassword, 12)
+    })
+    .then(hashedPassword => {
+      resetUser.password = hashedPassword
+      resetUser.resetToken = null
+      resetUser.resetTokenExpiration = null
+      return resetUser.save()
+    })
+    .then(() => {
+      res.redirect('/login')
     })
     .catch(err => {
       console.log(err)
