7767c15564e253c7acf687967328fe8c5a6c4fb7
Tiago Gomes <tidgomes@gmail.com>
2023-03-02 10:25:08 -0300

    handle eventual errors from the validation in the add product and edit product routes

--- controller/admin.js
+++ controller/admin.js
@@ -1,10 +1,13 @@
+const { validationResult } = require('express-validator/check')
 const Product = require('../models/product')
 
 exports.getAddProduct = (req, res) => {
   res.render('admin/edit-product', {
     pageTitle: 'Add Product',
     path: '/admin/add-product',
-    editing: false
+    editing: false,
+    hasError: false,
+    errorMessage: null
   })
 }
 
@@ -13,6 +16,19 @@ exports.postAddProduct = (req, res) => {
   const price = req.body.price
   const description = req.body.description
   const imageUrl = req.body.imageUrl
+  const errors = validationResult(req)
+
+  if (!errors.isEmpty()) {
+    console.log(errors.array())
+    return res.status(422).render('admin/edit-product', {
+      path: '/admin/add-product',
+      pageTitle: 'Add Product',
+      editing: false,
+      hasError: true,
+      product: { title, imageUrl, price, description },
+      errorMessage: errors.array()[0].msg
+    })
+  }
 
   const product = new Product({
     title,
@@ -58,7 +74,9 @@ exports.getEditProduct = (req, res) => {
         pageTitle: 'Edit Product',
         path: null,
         product,
-        editing
+        editing,
+        hasError: false,
+        errorMessage: null
       })
     })
     .catch(err => {
@@ -73,6 +91,25 @@ exports.postEditProduct = async (req, res) => {
   const updatedPrice = req.body.price
   const updatedDescription = req.body.description
   const updatedImageUrl = req.body.imageUrl
+
+  const errors = validationResult(req)
+
+  if (!errors.isEmpty()) {
+    console.log(errors.array())
+    return res.status(422).render('admin/edit-product', {
+      path: null,
+      pageTitle: 'Edit Product',
+      product: {
+        title: updatedTitle,
+        imageUrl: updatedImageUrl,
+        price: updatedPrice,
+        description: updatedDescription
+      },
+      editing: true,
+      hasError: true,
+      errorMessage: errors.array()[0].msg
+    })
+  }
 
   Product.findById(prodId)
     .then(product => {
